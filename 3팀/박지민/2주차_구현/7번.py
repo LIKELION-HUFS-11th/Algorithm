6a57a733674201fd8ad9ee291a96a14cd8f51014
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2023-05-12 12:48:43 +0900

    2주차

---
+++ 3팀/박지민/2주차_구현/7번.py
@@ -0,0 +1,18 @@
+#string으로 입력받기
+n = input()
+
+
+sum_1, sum_2=0,0
+
+#럭키 스트레이트 사용할 수 있는 조건
+for i in range (len(n)//2):
+    sum_1 += int(n[i])
+
+for j in range (len(n)//2,len(n)):
+    sum_2 += int(n[j])
+
+if sum_1 == sum_2:
+    print("LUCKY")
+    
+else:
+    print("READY")
