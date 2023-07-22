7594c5478ff4527f9bd0ebd83394f6032c866b1f
yoojung.choi <green980611@naver.com>
2023-07-23 00:29:54 +0900

    7주차 과제

---
+++ 3팀/최유정/week7_DP/assignment/0725_Q34.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] <= arr[j]:
+            dp[i] = max(dp[j]+1, dp[i])
+
+print(n-max(dp))
