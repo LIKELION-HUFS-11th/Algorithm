86042f79e688aeda0709b846cc94eca2668651b2
Minhyung Cho <dup33333@hufs.ac.kr>
2024-01-16 18:46:33 +0900

    10825 solved

--- 2팀/조민형/baekjoon_10825.py
+++ 2팀/조민형/baekjoon_10825.py
@@ -15,28 +15,45 @@ for i in range(studentNum):
                 break
             elif int(studentNameAndScore[1]) == int(result[j][1]):
                 k = j
-                while k < len(result) and int(studentNameAndScore[1]) == int(result[k][1]):
-                    if int(studentNameAndScore[2]) < int(result[k][2]):
+                while k < len(result):
+                    if int(studentNameAndScore[2]) > int(result[k][2]) and int(studentNameAndScore[1]) == int(result[k][1]):
+                        k+=1
+                        continue
+                    elif int(studentNameAndScore[2]) > int(result[k-1][2]) and int(studentNameAndScore[1]) == int(result[k-1][1]):
                         result.insert(k, studentNameAndScore)
                         break
                     elif int(studentNameAndScore[2]) == int(result[k][2]):
                         l = k
-                        while l < len(result) and int(studentNameAndScore[2]) == int(result[l][2]):
-                            if int(studentNameAndScore[3]) > int(result[l][3]):
+                        while l < len(result):
+                            if int(studentNameAndScore[3]) > int(result[l][3]) and int(studentNameAndScore[2]) == int(result[l][2]):
                                 result.insert(l, studentNameAndScore)
                                 break
                             elif int(studentNameAndScore[3]) == int(result[l][3]):
                                 a = l
-                                while a < len(result) and int(studentNameAndScore[3]) == int(result[a][3]):
-                                    if studentNameAndScore[0] < result[a][0]:
+                                while a < len(result):
+                                    if studentNameAndScore[0] < result[a][0] and int(studentNameAndScore[3]) == int(result[k][3]):
                                         result.insert(a, studentNameAndScore)
                                         break
                                     elif a == len(result)-1:
-                                        result.append(studentNameAndScore)                                
+                                        result.append(studentNameAndScore)
+                                        break                             
                                     a += 1
+                                break
+                            elif l == len(result)-1:
+                                result.append(studentNameAndScore)
+                                break                             
                             l+=1
+                        break
+                    elif k == len(result)-1:
+                        result.append(studentNameAndScore)
+                        break
                     k+=1
+                break
+            elif j == len(result)-1 :
+                result.append(studentNameAndScore)
+                break
             j+=1
+
 
 for b in range(len(result)):
     print(result[b][0])
