99684a98b27430c22da61d88792b8fc52b650a8b
Minhyung Cho <dup33333@hufs.ac.kr>
2024-01-29 16:01:20 +0900

    77

--- 2팀/조민형/76_minimum-window-substring.py
+++ 2팀/조민형/76_minimum-window-substring.py
@@ -39,54 +39,54 @@ class Solution:
 
 
 
-# from collections import Counter
+from collections import Counter
 
-# class Solution:
-#     def minWindow(self, s: str, t: str) -> str:
-#         def check_letter(s, t):
-#             s_dict = Counter(s)
-#             # t의 모든 문자가 s에 포함되어 있는지 확인
-#             for char in t:
-#                 if s_dict[char] - t_dict[char] < 0:
-#                     return False
-#             return True
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def check_letter(s, t):
+            s_dict = Counter(s)
+            # t의 모든 문자가 s에 포함되어 있는지 확인
+            for char in t:
+                if s_dict[char] - t_dict[char] < 0:
+                    return False
+            return True
         
-#         t_dict = Counter(t)
-#         i = len(t)-1
-#         j=0
-#         result = ""
+        t_dict = Counter(t)
+        i = len(t)-1
+        j=0
+        result = ""
     
-#          # 필요한 수와 총 문자의 길이가 같고 정렬해봤을떄 같으면 -> 바로 s 리턴
-#         if len(t)==len(s) and sorted(s) == sorted(t):
-#             return s
-#          # 필요한 문자열이 한개밖에 없으면 -> 하나만 in으로 찾은 뒤 바로 리턴
-#         elif len(t)==1 and t in s:
-#             return t
-#          # 필요한 문자열이 전체 문자열보다 길 경우 -> 어차피 성립이 안되므로 빈 문자열 리턴
-#         elif len(t)>len(s):
-#             return ""
-#         # 조건을 다 지나고 난 뒤에 나머지 조건들의 경우
-#         while i<=len(s):
-#              #i가 맨 끝에 도달할 때까지 반복하면서 해당 문자열과 찾는 문자열을 check_letter에 돌린 뒤 참이 나오면
-#             if check_letter(s[j:i],t_dict) == True:
-#                 # 결과가 만약에 저장되어 있는 상태 -> 길이 비교 후 더 작으면 결과도 없데이트
-#                 if result and len(result) > len(s[j:i]):
-#                     result = s[j:i]
-#                     j+=1
-#                     continue
-#                  # 아니라면 그냥 왼쪽 포인터만 오른쪽으로 옮김
-#                 elif result:
-#                     j+=1
-#                     continue
-#                 # 문자열을 아직 한 번도 못찾았을 때 쓰이는 조건
-#                 result = s[j:i]
-#                 j+=1
-#              #조건을 통과하지 못함 -> 다시 오른쪽으로 포인터를 옮겨서 찾아봐야 함
-#             else:
-#                 i+=1
-#          # 결과가 저장되어 있을 경우
-#         if result:
-#             return result
-#          # 결과가 저장되어 있지 않을 경우 -> 빈 문자열 리턴
-#         else: return ""
+         # 필요한 수와 총 문자의 길이가 같고 정렬해봤을떄 같으면 -> 바로 s 리턴
+        if len(t)==len(s) and sorted(s) == sorted(t):
+            return s
+         # 필요한 문자열이 한개밖에 없으면 -> 하나만 in으로 찾은 뒤 바로 리턴
+        elif len(t)==1 and t in s:
+            return t
+         # 필요한 문자열이 전체 문자열보다 길 경우 -> 어차피 성립이 안되므로 빈 문자열 리턴
+        elif len(t)>len(s):
+            return ""
+        # 조건을 다 지나고 난 뒤에 나머지 조건들의 경우
+        while i<=len(s):
+             #i가 맨 끝에 도달할 때까지 반복하면서 해당 문자열과 찾는 문자열을 check_letter에 돌린 뒤 참이 나오면
+            if check_letter(s[j:i],t_dict) == True:
+                # 결과가 만약에 저장되어 있는 상태 -> 길이 비교 후 더 작으면 결과도 없데이트
+                if result and len(result) > len(s[j:i]):
+                    result = s[j:i]
+                    j+=1
+                    continue
+                 # 아니라면 그냥 왼쪽 포인터만 오른쪽으로 옮김
+                elif result:
+                    j+=1
+                    continue
+                # 문자열을 아직 한 번도 못찾았을 때 쓰이는 조건
+                result = s[j:i]
+                j+=1
+             #조건을 통과하지 못함 -> 다시 오른쪽으로 포인터를 옮겨서 찾아봐야 함
+            else:
+                i+=1
+         # 결과가 저장되어 있을 경우
+        if result:
+            return result
+         # 결과가 저장되어 있지 않을 경우 -> 빈 문자열 리턴
+        else: return ""
     
