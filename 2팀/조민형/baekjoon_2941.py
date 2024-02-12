67c5712a9b0b47d3b39c7ce46d51bac339daaf9f
Minhyung Cho <dup33333@hufs.ac.kr>
2024-02-12 13:57:13 +0900

    2941 solved

--- 2팀/조민형/baekjoon_2941.py
+++ 2팀/조민형/baekjoon_2941.py
@@ -1,28 +1,22 @@
 import sys
 string = sys.stdin.readline()
-cr_list_two = ['lj','nj']
-cr_list_one = ['c=', 'c-','d-', 's=','z=']
+cr_list_one = ['c=', 'c-','d-', 's=','z=','lj','nj']
 
 result = 0
 left= 0
-right=2
+right=1
 while right<len(string):
-    print(string[left:right])
-    if right < len(string)-1 and string[left:right+1]=="dz=":
+    if right < len(string)-2 and string[left:right+2]=="dz=":
         left +=3
         right +=3
         result +=1
-    elif string[left:right] in cr_list_two:
-        left += 2
-        right += 2 
-        result+=1
-    elif string[left:right] in cr_list_one:
+    elif right < len(string)-1 and string[left:right+1] in cr_list_one:
         left +=2
         right+=2
         result+=1
     else:
         left += 1
-        right +=1
+        right+=1
         result+=1
 
 print(result)
