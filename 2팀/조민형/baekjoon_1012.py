25f185fc0498b72a397115f9b981546f07e3a35b
Minhyung Cho <dup33333@hufs.ac.kr>
2024-03-08 19:31:47 +0900

    1012 description add

--- 2팀/조민형/baekjoon_1012.py
+++ 2팀/조민형/baekjoon_1012.py
@@ -22,10 +22,13 @@ def explore(x, y):
 T = int(sys.stdin.readline())
 for _ in range(T):
     M, N, K = map(int, sys.stdin.readline().split())
+    # 가로, 세로 사이즈로 배추밭 만들기
     bc_list = [[0 for _ in range(M)] for _ in range(N)]
+    # 해당 배추 좌표 받아서 1로 처리해놓기
     for _ in range(K):
         bc_y, bc_x = map(int, sys.stdin.readline().split())
         bc_list[bc_x][bc_y] = 1
+    # 처음부터 모두 탐색하면서 1이 있는 곳을 찾으면 그대로 dfs 실행 -> 이어진 배추위치 전부 탐색
     count = 0
     for i in range(len(bc_list)):
         for j in range(len(bc_list[i])):
