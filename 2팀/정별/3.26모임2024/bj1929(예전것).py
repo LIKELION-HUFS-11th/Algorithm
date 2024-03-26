d1d47bd3f1747c0721640e8fbde56a48a7a12a7d
BYOUL JUNG <jbjb7458@gmail.com>
2024-03-26 19:08:41 +0900

    Add files via upload

---
+++ 2팀/정별/3.26모임2024/bj1929(예전것).py
@@ -0,0 +1,33 @@
+#700ms
+#M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+import sys
+m, n = map(int, sys.stdin.readline().split())
+
+# for i in range(m, n+1):
+    
+#     flag = 0
+#     for j in range(2, i):
+#         if i % j == 0: #합성수면
+#             flag +=1
+#             break
+#     if flag >0:
+#         continue
+#     print(i)
+
+nAr = [0] * (n+1)
+
+for i, elem in enumerate(nAr):
+
+    if elem or i == 0 or i == 1: #elem이 1이상이면
+        continue #다음 i로 넘어감
+    for j in range(1, n//i +1):
+        if j == 1:
+            continue
+        nAr[i * j] +=1
+
+for i, elem in enumerate(nAr):
+    if i< m or i == 1:
+        continue
+    if not elem: #elem이 0인 애들
+        print(i)
+    
--- 2팀/정별/3.26모임2024/bj2776.py
+++ 2팀/정별/3.26모임2024/bj2776.py
@@ -1,72 +1,39 @@
-# def solve(n):
-#   """
-#   현석이를 만족시키면서 정수 1, 2, ..., N을 선물하는 방법을 찾습니다.
+import sys
 
-#   Args:
-#     n: 정수의 개수 (1 < N < 5000)
+T = int(input())
 
-#   Returns:
-#     (성공 여부, 선물 순서)
-#   """
+N = int(input())
+Note1 = list(map(int, sys.stdin.readline().split()))
 
-#   # 1부터 N까지의 합
-#   Sn = n*(1+n)//2
+M = int(input())
+Note2 = list(map(int, sys.stdin.readline().split()))
 
-#   # Miller-Rabin 소수 판별법
-#   def is_prime(n):
-#     if n <= 1:
-#       return False
+Note1.sort()
 
-#     if n <= 3:
-#       return True
+# for elem in Note2:
+#     if elem in Note1:
+#         print(1)
+#     else:
+#         print(0)
 
-#     if n % 2 == 0 or n % 3 == 0:
-#       return False
-
-#     i = 5
-#     while i * i <= n:
-#       if n % i == 0 or n % (i + 2) == 0:
-#         return False
-#       i += 6
-
-#     return True
-
-#   # 선물 순서를 저장할 리스트
-#   order = []
-
-#   # 1부터 N까지의 정수를 리스트에 저장합니다.
-#   numbers = list(range(1, n + 1))
-
-#   # 리스트가 비어질 때까지 반복합니다.
-#   while numbers:
-#     # 리스트의 첫 번째 수를 제거하고 현석이에게 선물합니다.
-#     order.append(numbers.pop(0))
-
-#     # 리스트의 다음 수를 선택합니다.
-#     next_number = None
-#     for number in numbers:
-#       if not is_prime(number):
-#         if next_number is None or number > next_number:
-#           next_number = number
-
-#     # 리스트에서 다음 선물 번호를 제거합니다.
-#     if next_number is not None:
-#       numbers.remove(next_number)
-
-#   # 현석이를 만족시키면서 선물하는 방법이 있다면 "YES"를, 그렇지 않다면 "NO"를 출력합니다.
-#   if len(order) == n:
-#     return "YES", order
-#   else:
-#     return "NO", []
-
-# # 입력
-# n = int(input())
-
-# # 출력
-# success, order = solve(n)
-# print(success)
-# if success == "YES":
-#   print(" ".join(map(str, order)))
-
-
+for i in range(M):
+    target = Note2[i]
+    
+    l = 0
+    r = N-1
+    result = -1 #찾고자 하는 인덱스 번호
+    
+    while l <= r:
+        mid = (l+r)//2
+        if target == Note1[mid]:
+            result = mid
+            break
+        elif target > Note1[mid]:
+            l = mid +1
+        else:
+            r = mid-1
+    if result != -1: #바뀌었ㅇ다면
+        print(1)
+    else:
+        print(0)
 
