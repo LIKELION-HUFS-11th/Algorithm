6471fc9665e5439e880aefa8b2e251c03bc8c79f
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2024-05-20 20:48:39 +0900

    .

--- 2팀/정별/5.14모임2024/bj2251.py
+++ 2팀/정별/5.14모임2024/bj2251.py
@@ -12,7 +12,7 @@ nCheck = [[[0] * 201 for _ in range(201)] for _ in range(201)]
 def bfs(a, b, c): #매개변수 a,b,c는 A,B,C 컵에 남아있는 물의양임
     if nCheck[a][b][c]: #이미 계산했던 상태라면 복귀
         return
-    
+
     nCheck[a][b][c] = True
     
     if a == 0 and nVisited[c] == 0: #답 도출 조건. A가 비어있으면.
@@ -41,9 +41,6 @@ nAns.sort() #오름차순 정렬
 for elem in nAns:
     print(elem, end=' ')
     
-    
-    
-
 
 # nC = 0 #남아있는 c용량
 # nArray = []
@@ -59,7 +56,6 @@ for elem in nAns:
 #     nC.append(nArray)
 
 
-   
 #올바른 3차원 배열 설정 방법
 # nCheck = [[[0] * 201]*201]*201
 # 이 방법은 3차원 리스트를 만들되, 내부 리스트가 모두 같은 객체로 참조되기 때문에 문제를 일으킬 수 있습니다. 
@@ -70,7 +66,6 @@ for elem in nAns:
 
 # nCheck = [[[0] * 201 for _ in range(201)] for _ in range(201)]
 # 이 변경으로 인해 nCheck 리스트 내의 각 위치는 서로 독립적인 객체를 참조하게 되어 중복된 상태를 올바르게 체크할 수 있게 됩니다. 이는 코드의 정확성을 보장하는데 중요한 요소입니다. 따라서 이 변경을 통해 코드의 정확성이 향상되었고, 올바른 답을 반환할 수 있었습니다.
-
 
 
 
